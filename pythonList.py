a209ddcb256d46ebcf7df105f6d50beec3649a10
Harsh <harshpandey2060@gmail.com>
2025-07-02 18:55:33 +0530

    python list

---
+++ pythonList.py
@@ -0,0 +1,16 @@
+# List
+
+items = [2,3,1,3,1,3,3]
+item2 = [3,4,2,3,4]
+
+print(items[1:]) # slicing returns a new list
+print(items[-3:])
+
+print(items + [134,34,2,3]) 
+
+item2[0] = 43
+item2.append(77)
+
+item2.append(2**4) # 2 power 4
+print(item2)
+
